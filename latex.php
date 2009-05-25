43c052036587f1f8c348cc95caf9df4fc0dee0c3
Andrew Stacey <stacey@math.ntnu.no>
2009-05-25 12:42:18 +0200

    Streamlined code slightly, implemented newcommand primitive

--- latex.php
+++ latex.php
@@ -1,30 +1,32 @@
 <?php
 
 $commands = array();
+$primitives = array();
 
-  function nexttok ($latex)
+  function nexttok (&$latex)
 {
 
   $firstchar = substr($latex,0,1);
-  $rest = substr($latex,1);
+  $latex = substr($latex,1);
 
   if ($firstchar)
     {
 
       if ($firstchar == "\\")
 	{
-	  $secondchar = substr($rest,0,1);
-	  $rest = substr($rest,1);
+	  $secondchar = substr($latex,0,1);
+	  $latex = substr($latex,1);
 
 	  if (preg_match('/[A-z]/',$secondchar))
 	    {
-	      preg_match('/^([a-z]*)(.*)/s',$rest, $matches);
+	      preg_match('/^([a-z]*)(.*)/s',$latex, $matches);
+	      $latex = $matches[2];
 
-	      return array($firstchar . $secondchar . $matches[1], $matches[2]);
+	      return $firstchar . $secondchar . $matches[1];
 	    }
 	  else
 	    {
-	      return array($firstchar . $secondchar, $rest);
+	      return $firstchar . $secondchar;
 	    }
 	}
       elseif ($firstchar == '{')
@@ -33,29 +35,30 @@ $commands = array();
 	  $c = 1;
 	  while($c > 0)
 	    {
-	      $token = $token . substr($rest,0,1);
-	      $rest = substr($rest,1);
+	      $token = $token . substr($latex,0,1);
+	      $latex = substr($latex,1);
 	      $test = str_replace(array('\{','\}'),'',$token);
        
 	      $c = substr_count($test, '{') - substr_count($test,'}');
 
 	    }
-	  return array($token, $rest);
+	  return $token;
 	}
       else
 	{
-	  return array($firstchar, $rest);
+	  return $firstchar;
 	}
     }
   else
     {
-      return array('','');
+      return '';
     }
 }
 
-function expandtok ($token,$stream)
+function expandtok ($token,&$latex)
 {
   global $commands;
+  global $primitives;
   // get first character of the token
   $firstchar = substr($token,0,1);
 
@@ -63,7 +66,12 @@ function expandtok ($token,$stream)
     {
       // command
       $command = substr($token,1);
-      if (array_key_exists($command,$commands))
+      if (array_key_exists($command,$primitives))
+	{
+	  // command is actually a "primitive" which is a PHP function called with the stream as its argument
+	  return array(1,$primitives[$command]($latex));
+	}
+      elseif (array_key_exists($command,$commands))
 	{
 	  // command is known
 	  $args = $commands[$command]["args"];
@@ -73,59 +81,58 @@ function expandtok ($token,$stream)
 	  // TODO: handle optional arguments
 	  // trim whitespace after command first
 	  // NB do this here to get '\)' correct.
-	  $stream = ltrim($stream);
+	  $latex = ltrim($latex);
 	  for ($i = 0;$i < $args;$i++)
 	    {
-	      list($arg,$stream) = nexttok($stream);
+	      $arg = nexttok($latex);
 	      $defn=str_replace("#" . ($i+1), $arg, $defn);
 	    }
 	  // return expanded command
-	  return array(1,$defn,$stream);
+	  return array(1,$defn);
 	}
       else
 	{
 	  // command is not known, just return it unexpanded
-	  return array(0,$token,$stream);
+	  return array(0,$token);
 	}
     }
   elseif ($firstchar == "{")
     {
       // token by virtue of grouping, strip off outermost grouping
       $extoken = preg_replace('/^{(.*)}$/s','$1',$token);
-      return array(1,$extoken,$stream);
+      return array(1,$extoken);
     }
   else
     {
       // no expansion to be done
       // TODO: add support for ^ and _ in math mode
-      return array(0,$token,$stream);
+      return array(0,$token);
     }
 }
 
-function processLaTeX ($latex)
+function processLaTeX (&$latex)
 {
   $processed = "";
 
   while ($latex)
     {
       // get next token from stream
-      list($token,$rest) = nexttok($latex);
+      $token = nexttok($latex);
 
       // expand token
-      list($mod,$extoken,$rest) = expandtok($token,$rest);
+      list($mod,$extoken) = expandtok($token,$latex);
 
       // did that actually do anything?
 
       if ($mod)
 	{
 	  // yes, reinsert at front of stream and start again
-	  $latex = $extoken . $rest;
+	  $latex = $extoken . $latex;
 	}
       else
 	{
 	  // no, consider as processed and pass on to the next chunk
 	  $processed = $processed . $extoken;
-	  $latex = $rest;
 	}
     }
   return $processed;
@@ -134,6 +141,8 @@ function processLaTeX ($latex)
 function newcommand ($name,$args,$opts,$defn)
 {
   global $commands;
+  // strip off slashes, just in case
+  $name=ltrim($name,"\\");
   $commands[$name] = array(
 			   "args" => $args,
 			   "opts" => $opts,
@@ -142,6 +151,50 @@ function newcommand ($name,$args,$opts,$defn)
 }
 
 // commands
+
+$primitives["newcommand"] = 
+  create_function ('& $latex','
+list($mod,$name) = expandtok(nexttok($latex),$latex); // first argument is name of command, need to strip off brackets
+$nexttok = nexttok($latex); // next is either defn or says we have arguments
+if ($nexttok == "[")
+  {
+    // have arguments, how many?
+    $num = nexttok($latex);
+    nexttok($latex); // ought to be a "]", should test this
+    // should test for numeric here also
+    $nexttok = nexttok($latex); // either optional first argument or defn
+    if ($nexttok == "[")
+      {
+	// option argument specified
+	$nexttok = nexttok($latex);
+	$opt = "";
+	while ($nexttok != "]")
+	  {
+	    // slurp everything up to but not including "]"
+	    $opt = $opt . $nexttok;
+	    $nexttok = nexttok($latex);
+	  }
+	$optarray = array($opt);
+	$defn = nexttok($latex);
+      }
+    else
+      {
+	// no optional arguments, just defn
+	$optarray = array();
+	$defn = $nexttok;
+      }
+  }
+else
+  {
+    $num = 0;
+    $optarray = array();
+    $defn = $nexttok;
+  }
+newcommand($name,$num,$optarray,$defn);
+	      return;
+');
+
+
 
 newcommand("emph",1,array(),"<i>#1</i>");
 newcommand("textbf",1,array(),"<strong>#1</strong>");
@@ -262,11 +315,15 @@ newcommand("omega",0,array(),"&omega;");
 
 // Main program starts here
 
-$source = stripslashes($_REQUEST["latex"]);
+
+// TODO: Is there a way to figure out whether or not stripslashes is needed?
+// $source = stripslashes($_REQUEST["latex"]);
+$source = $_REQUEST["latex"];
+
 
 ?>
 
-<form action="http://localhost/~astacey/latex.php" method="post">
+<form action="<?php print $_SERVER['PHP_SELF'] ?>" method="post">
 <p>
 <textarea name="latex" rows="20" cols="50">
 <?php print $source ?>
