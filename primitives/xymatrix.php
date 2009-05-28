38db067736e3233a6f25673e2fc600624099add7
Andrew Stacey <astacey@Thargelion>
2009-05-28 23:01:54 +0200

    More on xymatrix to SVG

--- primitives/xymatrix.php
+++ primitives/xymatrix.php
@@ -1,8 +1,8 @@
 // name: xymatrix
 $args = "";
 $dim = array(
-	     "row" => "20",
-	     "col" => "20"
+	     "row" => "2",
+	     "col" => "2"
 	     );
 // morally, \ifnextchar{@} ...
 $nexttok = nexttok($latex);
@@ -18,29 +18,70 @@ if ($args)
     $argarray = explode("@",$args);
     
   }
+// TODO: convert lengths (should be global function) 
 $latex = $nexttok . $latex;
 $matrix = nextgrp($latex);
 $svg = '<svg xmlns="http://www.w3.org/2000/svg">' . "\n";
+// build a matrix of entries
+$m = 0;
+$n = 0;
+$maxwidth = 1;
+$maxheight = 1.5;
 while ($matrix)
   {
-    $entry = "";
+    $entry[$m][$n] = "";
     $nexttok = nexttok($matrix);
     while ($nexttok and ($nexttok != '&') and ($nexttok != '\\\\'))
       {
-	$entry = $entry . $nexttok;
+	$entry[$m][$n] = $entry[$m][$n] . $nexttok;
 	$nexttok = nexttok($matrix);
       }
-    $svg .= '<foreignObject x="' . ($x * $dim["row"]) . '" y="' . ($y * $dim["col"]) . '">' . $entry . '</foreignObject>' . "\n";
     if ($nexttok == '&')
       {
-	$x++;
+	$n++;
       }
     elseif ($nexttok == '\\\\')
       {
-	$y++;
-	$x = 0;
+	$m++;
+	$n = 0;
+      }
+    $width = strlen($entry[$m][$n]); // TODO: replace by something better
+    if ($width > $maxwidth)
+      $maxwidth = $width;
+    // same for height
+  }
+$dim["row"] = $dim["row"] + $maxwidth;
+$dim["col"] = $dim["col"] + $maxheight;
+
+
+for($m = 0;$m < count($entry);$m++)
+  {
+    for($n = 0;$n < count($entry[$m]); $n++)
+      {
+	$svg = $svg
+	  . '<foreignObject x="'
+	  . ($n * $dim["row"])
+	  . 'em" y="'
+	  . ($m * $dim["col"])
+	  . 'em" width="'
+	  . $maxwidth
+	  . 'em" height="'
+	  . ($maxheight + 1)
+	  . 'em">'
+	  . '<body xmlns="http://www.w3.org/1999/xhtml"><div style="width:'
+	  . $maxwidth
+	  . 'em,height:'
+	  . $maxheight
+	  . 'em" align="center">'
+	  . $entry[$m][$n]
+	  . '</div></body>'
+	  . '</foreignObject>'
+	  . "\n";
       }
   }
 $svg = $svg . '</svg>';
+
 print '<pre>' . htmlspecialchars($svg) . '</pre>';
+
+$latex = $svg . "\0" . $latex;
 return;
